793356766705ae27e7f904a966615aecabcba881
Awijie138 <mutawallysyarawy@gmail.com>
2020-10-18 21:24:02 +0800

    First commit

---
+++ Assets/PlayAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	[RequireComponent(typeof(Collider))]
+public class PlayAnim : MonoBehaviour {
+	
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+		anim=GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void animHey(){
+		anim.Play("WAIT03/HeyAnim");
+	}
+	
+	public void animDefault(){
+		anim.Play("WAIT00");
+	}
+	
+	public void animInteract(){
+		anim.Play("DAMAGED00");
+	}
+	
+
+}
---
+++ Assets/SoldierPlayAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	[RequireComponent(typeof(Collider))]
+public class SoldierPlayAnim : MonoBehaviour {
+
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+		anim=GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	public void animSoldierHey(){
+		anim.Play("Idle_Shoot");
+	}
+	public void animSoldierInteract(){
+		anim.Play("Die");
+	}
+	public void animSoldierDefault(){
+		anim.Play("Idle_GunMiddle");
+	}
+	public void animSoldierMove(){
+		anim.Play("WalkForward_Shoot");
+	}
+
+}
